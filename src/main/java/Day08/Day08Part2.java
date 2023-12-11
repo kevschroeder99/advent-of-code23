eb7c2404998df57f6e65e20e309a612f8874260e
Schröder, Kevin <kevin.schroeder@hdi.de>
2023-12-11 23:28:10 +0100

    Day9 Part 1

--- src/main/java/Day08/Day08Part2.java
+++ src/main/java/Day08/Day08Part2.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Day08Part2 {
 
-    //private static String file = "src/main/resources/08_input.txt";
-    private static String file = "src/test/resources/08_input_test_part2.txt";
+    private static String file = "src/main/resources/08_input.txt";
+    //private static String file = "src/test/resources/08_input_test_part2.txt";
 
     public static void main(String[] args) throws Exception {
         Day08Part2 starter = new Day08Part2();
@@ -26,40 +25,77 @@ public class Day08Part2 {
             }
             String copyInstructions = list.get(0);
             Boolean isFinished = Boolean.FALSE;
-            int steps = 0;
-            Day08 starter = new Day08();
+            long steps = 0;
 
-            List<Node> nodes = starter.setNodes(list);
+            List<Node> nodes = setNodes(list);
             List<Node> endNodes = findAllEndNodes(nodes);
-            List<Node> startingNodes = findAllStartingNodes(nodes);
+            List<String> endNodesNames = getAllEndNodesNames(endNodes);
+            List<Node> currentNodes = findAllStartingNodes(nodes);
+
             while (!isFinished) {
                 if (list.get(0).isEmpty()) {
                     list.set(0, copyInstructions);
                 }
                 String instruction = list.get(0);
-                boolean nextStepIsLeft = starter.isNextStepLeft(instruction);
-                List<String> currentNodename = getCurrentNode(nextStepIsLeft, nodes, startingNodes);
+                boolean nextStepIsLeft = isNextStepLeft(instruction);
 
-
+                currentNodes = getCurrentNode(nextStepIsLeft, nodes, currentNodes);
+                List<String> currentNodename = getCurrentNodeNames(currentNodes);
+                steps++;
+                list.set(0, list.get(0).substring(1));
+                if (currentNodename.equals(endNodesNames)) {
+                    isFinished = Boolean.TRUE;
+                }
             }
+            System.out.println(steps);
         }
     }
 
-    public List<String> getCurrentNode(boolean isLeft, List<Node> nodes, List<Node> startingNodes) {
-        List<String> currentNodes = new ArrayList<>();
-        for (Node node : startingNodes) {
-            Optional<Node> currentNode = nodes.stream()
-                    .filter(obj -> obj.getName()
-                            .equals(node.getName()))
-                    .findFirst();
+    private List<String> getCurrentNodeNames(List<Node> currentNodes) {
+        List<String> currentNodeNames = new ArrayList<>();
+        for (Node node : currentNodes) {
+            currentNodeNames.add(node.getName());
+        }
+        return currentNodeNames;
+    }
 
+    public List<Node> getCurrentNode(boolean isLeft, List<Node> nodes, List<Node> currentNodes) {
+        List<Node> resultNodes = new ArrayList<>();
+        for (Node node : currentNodes) {
             if (isLeft) {
-                currentNodes.add(currentNode.get().getLeft());
+                String leftNode = node.getLeft();
+                Node nodeLeft = nodes.stream()
+                        .filter(obj -> obj.getName()
+                                .equals(leftNode))
+                        .findFirst()
+                        .get();
+                resultNodes.add(nodeLeft);
             } else {
-                currentNodes.add(currentNode.get().getRight());
+                String rightNode = node.getRight();
+                Node nodeRight = nodes.stream()
+                        .filter(obj -> obj.getName()
+                                .equals(rightNode))
+                        .findFirst()
+                        .get();
+                resultNodes.add(nodeRight);
             }
         }
-        return currentNodes;
+        return resultNodes;
+    }
+
+    private List<String> getAllEndNodesNames(List<Node> endNodes) {
+        List<String> endNodesNames = new ArrayList<>();
+        for (Node node : endNodes) {
+            endNodesNames.add(node.getName());
+        }
+        return endNodesNames;
+    }
+
+    public boolean isNextStepLeft(String instruction) {
+        if (instruction.startsWith("L")) {
+            return true;
+        }
+        return false;
     }
 
     private List<Node> findAllEndNodes(List<Node> nodes) {
@@ -80,5 +116,35 @@ public class Day08Part2 {
             }
         }
         return startingNodes;
+    }
+
+    public List<Node> setNodes(ArrayList<String> list) {
+        List<Node> nodesList = new ArrayList<>();
+        for (int i = 1; i < list.size(); i++) {
+            String s = list.get(i);
+            if (s != null && !s.isEmpty()) {
+                Node node = new Node();
+                //Set Name
+                setNodeName(node, s);
+                //Set Left & Right
+                setDirections(node, s);
+                nodesList.add(node);
+            }
+        }
+        return nodesList;
+    }
+
+    public void setDirections(Node node, String s) {
+        String[] leftAndRight = s.split(" = ");
+        String[] leftAndRightSplitted = leftAndRight[1].split(",");
+        String left = leftAndRightSplitted[0];
+        String right = leftAndRightSplitted[1];
+        node.setLeft(left.replaceAll("[^0-9A-Z]", ""));
+        node.setRight(right.replaceAll("[^0-9A-Z]", ""));
+    }
+
+    private void setNodeName(Node node, String s) {
+        String[] splitString = s.split(" = ");
+        node.setName(splitString[0]);
     }
 }
---
+++ src/main/java/Day09/Day09.java
@@ -0,0 +1,109 @@
+package Day09;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Day09 {
+    private static String file = "src/main/resources/09_input.txt";
+    //private static String file = "src/test/resources/09_input_test.txt";
+
+    public static void main(String[] args) throws Exception {
+        Day09 starter = new Day09();
+        starter.doPart1(file);
+    }
+
+    private void doPart1(String file) throws Exception {
+        FileReader fileReader = new FileReader(file);
+        try (BufferedReader br = new BufferedReader(fileReader)) {
+            String line;
+            List<Integer> values = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                int value = convertStringToArray(line);
+                values.add(value);
+            }
+            System.out.println(values);
+            System.out.println(values.stream().mapToInt(i -> i).sum());
+        }
+    }
+
+    private int convertStringToArray(String line) {
+        String[] stringArray = line.split("\\s+");
+        List<Integer> startingList = new ArrayList<>();
+        for (String s : stringArray) {
+            startingList.add(Integer.parseInt(s));
+        }
+        List<Integer> deltaList = startingList;
+        List<List<Integer>> historyList = new ArrayList<>();
+        historyList.add(startingList);
+        //Solange Listen erstellen, bis nur nullen in der Liste sind.
+        while (!allElementAreZeros(deltaList)) {
+            deltaList = getDeltasInStartingList(deltaList);
+            historyList.add(deltaList);
+        }
+        //Von unten auffüllen:
+        //1. eine Null einfügen
+        //2. Zahl dadrüber einfügen
+        //3. Zahl aus 2. + Zahl dadrüber = neue Zahl
+        //4. weiter
+        int lowestList = historyList.size();
+        for (int i = lowestList - 1; i >= 0; i--) {
+            historyList.get(i);
+            if (i == lowestList - 1) {
+                addValuesToList(historyList, i, true);
+            } else {
+                addValuesToList(historyList, i, false);
+            }
+        }
+        return getHistoryValues(historyList);
+    }
+
+    private int getHistoryValues(List<List<Integer>> historyList) {
+        List<Integer> highestList = historyList.get(0);
+        int size = highestList.size();
+        return highestList.get(size - 1);
+    }
+
+    private void addValuesToList(List<List<Integer>> integers, int i, boolean isLowestList) {
+        if (isLowestList) {
+            integers.get(i).add(0);
+        } else {
+            int sizeCurrentList = integers.get(i).size();
+            int sizeNextList = integers.get(i + 1).size();
+            int valueCurrentList = integers.get(i).get(sizeCurrentList - 1);
+            int valueNextList = integers.get(i + 1).get(sizeNextList - 1);
+            integers.get(i).add(valueNextList + valueCurrentList);
+        }
+    }
+
+    private boolean allElementAreZeros(List<Integer> deltaList) {
+        for (Integer i : deltaList) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> getDeltasInStartingList(List<Integer> startingList) {
+        List<Integer> deltaList = new ArrayList<>();
+        for (int i = 0; i < startingList.size() - 1; i++) {
+            Integer value1 = startingList.get(i);
+            Integer value2 = startingList.get(i + 1);
+            deltaList.add(value2 - value1);
+        }
+        return deltaList;
+    }
+
+    private boolean allValuesAreZeros(List<Integer> deltaList) {
+        AtomicBoolean allValuesAreZeros = new AtomicBoolean(true);
+        deltaList.stream().forEach(i -> {
+            if (i != 0) {
+                allValuesAreZeros.set(false);
+            }
+        });
+        return allValuesAreZeros.get();
+    }
+}
